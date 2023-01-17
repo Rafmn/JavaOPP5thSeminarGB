5d388ae7d44b2d25345e4646245af3c45e66183f
Marat <email.marat@gmail.com>
2023-01-17 20:43:05 +0300

    First commit

---
+++ Rational.java
@@ -0,0 +1,25 @@
+class Rational extends Numbers{
+    double real; // реальная часть
+
+
+    public Rational(double real) {
+        this.real = real;
+
+    }
+
+    public Rational() {
+//        this(0);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+
+
+
+}
